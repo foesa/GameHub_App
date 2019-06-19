43243c8fe176a9903a34c59c658fe56e10ffa545
Foesa <leguavoen@gmail.com>
2019-06-19 01:09:22 +0100

    Created Launcher and Game class. Need to do main tho and pickle etc

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import os
 
 def main():
-    filepath = input("Please enter the filepath for games to be found: ")
-    walker(filepath)
+   filepath = input("Please enter the filepath for games to be found: ")
+   filepath = filepath.replace("\\", "/")
+   filepath = filepath.replace('"',"")
+   print(filepath)
+   walker(filepath)
 
 def walker(filepath):
     for folderName, subfolders, filenames in os.walk(filepath):
@@ -15,4 +18,12 @@ def walker(filepath):
 if __name__ == "__main__":
     main()
 
-print("Guru99")
+"""
+TODO
+Get info on list of launchers to compare names to
+Create List of launchers based of .exe ending and is on Launcher list
+Add games to Launchers
+Display in console to show it works
+Pickle all the stuff
+Make UI using Kivy or something similar
+"""
